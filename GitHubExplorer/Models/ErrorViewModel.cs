b58a5a821f293c3dab09913776a1e9daed6738da
kristall268 <kristall268@outlook.com>
2026-04-29 17:37:26 +0500

    New .NET project

---
+++ GitHubExplorer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GitHubExplorer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
